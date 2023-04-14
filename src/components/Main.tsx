acf8b34013318d4cc286f2df9a796069a52f16c0
eloy <eloy.ricardo.550@gmail.com>
2023-04-14 16:09:51 -0300

    [ADD] styling classNames

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,21 @@
 import { memo } from 'react';
 import { useFetchCat } from '../hooks/useFetchCat';
+import '../styles/Main.css';
 
 function Main() {
-  const [data, loading, error] = useFetchCat('https://catfact.ninja/fact');
+  const [data, loading, error] = useFetchCat();
 
   return (
-    <main>
+    <main className='centeredContent'>
       <h1>Cat facts</h1>
       {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
+      {error && <p>Error</p>}
       {data.length > 0 && (
-        <div>
+        <div className='centeredContent'>
           <p>{data.fact}</p>
           <img
             src={data.img}
-            alt='cat pic'
+            alt='cat pic using the first word of the fact'
           />
         </div>
       )}
--- src/hooks/useFetchCat.ts
+++ src/hooks/useFetchCat.ts
@@ -6,28 +6,59 @@ export interface Cat {
   img?: string;
 }
 
-export const useFetchCat = (url: string): [Cat, boolean, boolean] => {
+export interface CatImage {
+  tags: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  _id: string;
+  validated: boolean;
+  owner: string;
+  file: string;
+  mimetype: string;
+  size: number;
+  url: string;
+}
+
+const CAT_IMAGE_API = (firstWord: string) => `https://cataas.com/cat/says/${firstWord}?json=true`
+const CAT_PREFIX_URL = 'https://cataas.com'
+const CAT_FACT_API: string = 'https://catfact.ninja/fact'
+
+export const useFetchCat = (): [Cat, boolean, boolean] => {
   const [error, setError] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<Cat>({ fact: '', length: 0, img: '' });
 
   useEffect(() => {
-    async function getData(url: string) {
+    // Fetch function
+    async function getData<t>(url: string): Promise<t> {
       const response = await fetch(url);
-      const data: Cat = await response.json();
-      const wordEndIndex = data.fact.indexOf(' ') + 1;
+      const data: t = await response.json();
 
-      const catData: Cat = {
-        ...data,
-        img: `https://cataas.com/cat/says/${data.fact
-          .toLowerCase()
-          .slice(0, wordEndIndex)}`,
-      };
-
-      return catData;
+      return data;
     }
 
-    getData(url)
+    // Get cat image data
+    async function getImageData(firstWord: string) {
+      const catImageData: CatImage = await getData(CAT_IMAGE_API(firstWord));
+      const catImageUrl = `${CAT_PREFIX_URL}${catImageData.url}`;
+
+      return catImageUrl;
+    }
+    // Get cat fact data
+    async function getCatData() {
+      const catData: Cat = await getData(CAT_FACT_API);
+
+      const { fact } = catData
+      const firstWordOfFact = fact.split(' ')[0].toLowerCase()
+
+
+      const catImageUrl = await getImageData(firstWordOfFact);
+
+      return { ...catData, img: catImageUrl }
+    }
+
+    // Get final data
+    getCatData()
       .then((data: Cat) => {
         if (data.length > 0) {
           setData(data);
@@ -35,11 +66,11 @@ export const useFetchCat = (url: string): [Cat, boolean, boolean] => {
           setError(true);
         }
       })
-      .catch((error: boolean) => {
-        setError(error);
+      .catch(() => {
+        setError(true);
       })
       .finally(() => setLoading(false));
-  }, [url]);
+  }, []);
 
   return [data, loading, error];
 };
