6731afaf4bcbc01824cde435324d4eae6edfb9bc
eloy <eloy.ricardo.550@gmail.com>
2023-04-16 15:32:06 -0300

    [ADD] button to get new fact and image

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,18 @@
-import { useFetchFact } from '../hooks/useFetchFact';
-import { useFetchCatImage } from '../hooks/useFetchCatImage';
+import { useCatFact } from '../hooks/useCatFact';
+import { useCatImage } from '../hooks/useCatImage';
+import { getCatFact } from '../services/getCatFact';
 import '../styles/Main.css';
 
 function Main() {
-  const { fact, factError } = useFetchFact();
-  const { catImage,loading, imageError } = useFetchCatImage(fact);
+  const { fact, refreshRandomFact, factError } = useCatFact();
+  const { catImage, loading, imageError } = useCatImage(fact);
 
-  
+  const handleClick = refreshRandomFact
+
   return (
     <main className='centeredContent'>
       <h1>Cat facts</h1>
+      <button onClick={handleClick}>Get a new fact</button>
       {loading && !(factError || imageError) && <p>Loading...</p>}
       {(factError || imageError) && <p>Error:{factError || imageError}</p>}
       {!loading && !(factError || imageError) && fact && (
